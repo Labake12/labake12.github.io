648e8b00e36506a55073b878c7e79a7da5f672b5
Labake12 <101086212+Labake12@users.noreply.github.com>
2022-04-04 16:04:18 +0100

    Add files via upload

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,6 +1,7 @@
 var operator = prompt('enter operator (either +, -, * or /):');
 const firstNumber = prompt('enter first number:');
 const secondNumber = prompt('enter second number');
+let result;
 
 if (operator === '+') {
     result = firstNumber + secondNumber;
@@ -11,7 +12,10 @@ else if (operator === '-') {
 else if (operator === '*') {
     result = firstNumber * secondNumber;
 }
-else {
+else if (operator === '/'){
     result = firstNumber / secondNumber;
 }
-console.log('Result is ' + result);
+else {
+    alert('invalid input')
+}
+console.log('Result is ' + result)
